4d6ab0a35e4b3b3d4a540c87689930d93f8c8a84
hwangyunho <hwang9yh@wemakeprice.com>
2021-05-06 23:53:28 +0900

    chore: 피보나치 테스트 케이스 추가

---
+++ src/test/java/fibonacci/FibonacciTest.java
@@ -0,0 +1,27 @@
+package fibonacci;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class FibonacciTest {
+
+    @DisplayName("피보나치 반복")
+    @ParameterizedTest
+    @CsvSource(value = {"1:1", "2:1", "3:2", "4:3", "5:5"}, delimiter = ':')
+    void fibonacci_반복문_Test(int number, int expected) {
+        Fibonacci fibonacci = new Fibonacci();
+        assertThat(fibonacci.fibonacci(number)).isEqualTo(expected);
+    }
+
+    @DisplayName("피보나치 재귀")
+    @ParameterizedTest
+    @CsvSource(value = {"1:1", "2:1", "3:2", "4:3", "5:5"}, delimiter = ':')
+    void fibonacci_재귀함수_Test(int number, int expected) {
+        Fibonacci fibonacci = new Fibonacci();
+        assertThat(fibonacci.recursiveFibonacci(number)).isEqualTo(expected);
+    }
+}
