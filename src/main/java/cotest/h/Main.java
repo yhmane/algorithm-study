4e6f9e23a8e7d7268cf11ce41c2689b4057cbe5e
황윤호 <hwang9yh@wemakeprice.com>
2022-06-13 17:10:39 +0900

    백준 코테 입력 포맷

---
+++ src/main/java/cotest/h/Main.java
@@ -0,0 +1,13 @@
+package cotest.h;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
